a5bea5afb5af7c95a7604de87a8e9fcacd8e3545
YeonGyu-Kim <62788631+YeonGyu-Kim@users.noreply.github.com>
2020-05-17 21:18:48 +0900

    Complete todolist
    The End!

--- todo.js
+++ todo.js
@@ -1,5 +1,5 @@
-const toDoFrom = document.querySelector(".js-toDoFrom");
-const toDoInput = toDoFrom.querySelector("input");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
@@ -61,7 +61,7 @@ function loadToDos() {
 
 function init() {
   loadToDos();
-  toDoFrom.addEventListener("submit", handleSubmit);
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
